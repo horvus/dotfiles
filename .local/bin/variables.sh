367722e02b824adfb82c27d2d0d04c45bb4f54c5
horvus <42658913+horvus@users.noreply.github.com>
2019-05-23 00:54:31 -0400

    scripts

---
+++ .local/bin/variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/fish
+
+echo "You launched $0, with $argv parameters"
+echo "First parameter is $1"
+
+# this script is just a test,
+# it has no utility whatsoever
---
+++ .local/bin/volume.py
@@ -0,0 +1,8 @@
+import alsaaudio as alsa
+from time import sleep
+
+m = alsa.Mixer()
+vol = m.getvolume()
+
+print(' ' + str(vol[0]) + '%')
+sleep(0.1)
